e2d8c7bf64134b9d74e529bf1f9e896d23b8c7cf
whyeonc <48907109+whyeonc@users.noreply.github.com>
2024-08-08 15:45:46 +0200

    Delete pseudo-haploid_assembly directory

--- pseudo-haploid_assembly/step1.hifiasm.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-projectdir="/filer-dg/agruppen/dg2/cho/chamomile"
-
-reads=$projectdir/hifi_reads
-
-prefix=${projectdir:r}/chamomile
-
-find $reads | grep 'fastq.gz$' | xargs hifiasm -t 50 -o $prefix  2> ${prefix}_asm.err
